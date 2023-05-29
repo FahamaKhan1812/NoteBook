4f61ce5dc1ad639d5244e51f9572264b5e06e7c3
fahama khan <fahamakhan990@gmail.com>
2023-05-29 12:52:22 +0500

    add base controller

---
+++ NoteBooks.Api/Controllers/v1/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NoteBook.DataService.IConfiguration;
+
+namespace NoteBooks.Api.Controllers.v1
+{
+    [Route("api/{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class BaseController : ControllerBase
+    {
+        public readonly IUnitOfWork _unitOfWork;
+
+        public BaseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
